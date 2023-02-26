303f2585ff46dd4ae1b20d14578c6685bd5ef760
khoakomlem <khoakomlem@gmail.com>
2023-02-27 00:44:40 +0700

    Bullet state hook

--- src/pixiApp/entity/Bullet.ts
+++ src/pixiApp/entity/Bullet.ts
@@ -2,9 +2,12 @@ import * as PIXI from 'pixi.js';
 import type {ITickData} from '@gunsurvival/core/types';
 import type * as WorldCore from '@gunsurvival/core/world';
 import type * as EntityCore from '@gunsurvival/core/entity';
+import type * as EntityServer from '@gunsurvival/server/entity';
 import Entity from './Entity.js';
+import {type DataChange} from '../../lib/colyseus.js';
 
 export default class Bullet extends Entity {
+	declare entityCore: EntityCore.Bullet;
 	displayObject = PIXI.Sprite.from('images/Bullet.png');
 
 	onAdd() {
@@ -14,9 +17,32 @@ export default class Bullet extends Entity {
 	}
 
 	update(world: WorldCore.default, tickData: ITickData) {
-		const angle = Math.atan2((this.entityCore as EntityCore.Bullet).vel.y, (this.entityCore as EntityCore.Bullet).vel.x);
+		const angle = Math.atan2((this.entityCore).vel.y, (this.entityCore).vel.x);
 		this.displayObject.rotation = angle;
 		this.displayObject.x = this.entityCore.body.pos.x;
 		this.displayObject.y = this.entityCore.body.pos.y;
+	}
+
+	hookStateChange(entityServer: EntityServer.default): void {
+		super.hookStateChange(entityServer);
+
+		(entityServer as EntityServer.Bullet).vel.onChange = (changes: DataChange[]) => {
+			changes.forEach((change: DataChange) => {
+				if (this.isPlayer) {
+					return;
+				}
+
+				switch (change.field) {
+					case 'x':
+						this.entityCore.vel.x = change.value as number;
+						break;
+					case 'y':
+						this.entityCore.vel.y = change.value as number;
+						break;
+					default:
+						break;
+				}
+			});
+		};
 	}
 }
