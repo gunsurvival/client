6df9bf05903f2a5360e65269c6c4f34ef679ec3d
Dau Khoa <khoakomlem@gmail.com>
2023-03-14 22:01:13 +0700

    Style healthbar

--- src/ui/HealthBar.tsx
+++ src/ui/HealthBar.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function HealthBar() {
 	return (
 		<div className='flex -translate-x-2'>
-			<div className='w-14 h-14 rounded-full relative left-3 border-solid border-green-900 border-8'>
-				<img src='./images/heart.png' />
+			<div className='w-12 h-12 rounded-full relative left-3 border-solid border-red-600 border-8 bg-red-900 flex justify-center'>
+				<img src='./images/heart.png' className='w-4/5' />
 			</div>
 			<div className='flex flex-wrap content-center'>
-				<div className='w-64 h-10 bg-green-900 rounded-lg'></div>
+				<div className='w-48 h-7 bg-red-600 rounded-lg'></div>
 			</div>
 		</div>
 	);
