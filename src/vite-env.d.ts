9b8cb8ce98922e6fa6a83f2ec7d55272a290b132
Đậu Khoa <khoakomlem@gmail.com>
2023-07-29 18:53:44 +0700

    feat: export types for 'pixi-viewport' (module res: node)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="vite/client" />
+declare module 'pixi-viewport/dist/pixi_viewport.js' {
+	export * from 'pixi-viewport/dist/global.js';
+	export * from 'pixi-viewport/dist/Viewport.js';
+	export * from 'pixi-viewport/dist/plugins/index.js';
+	export * from 'pixi-viewport/dist/ease.js';
+	export * from 'pixi-viewport/dist/InputManager.js';
+	export * from 'pixi-viewport/dist/PluginManager.js';
+	export * from 'pixi-viewport/dist/Viewport.js';
+}
